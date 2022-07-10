ed9f44ba81631bd0b206d318532319f4eb0d8407
sn-911 <512428493@qq.com>
2022-07-10 13:19:21 +0800

    add

--- 1_算法示例/downlaod_dataset.sh
+++ 1_算法示例/downlaod_dataset.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 wget -L http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar
-unzip VOCtrainval_11-May-2012.zip
+tar -xvf VOCtrainval_11-May-2012.tar
 mkdir data
 mv VOCdevkit/VOC2012 data/
-rm -f VOCtrainval_11-May-2012.zip
+rm -rf VOCdevkit
+rm -f VOCtrainval_11-May-2012.tar
